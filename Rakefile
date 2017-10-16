83fa4072f8b192ea223c2ea1639597b0942069bb
Annette Maza <nettymaza@Annettes-MacBook.local>
2017-10-16 17:41:45 -0600

    Done.

--- Rakefile
+++ Rakefile
@@ -5,3 +5,10 @@ require 'sinatra/activerecord/rake'
 
 
 # Type `rake -T` on your command line to see the available rake tasks.
+task :migrations do
+  system('rake db:migrate && rake db:migrate SINATRA_ENV=test')
+end
+
+task :drop do
+  system('rm db/nycdevelopment.sqlite && rm db/nyctest.sqlite && rm db/schema.rb')
+end
--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class FiguresController < ApplicationController
 
+  get '/' do
+    redirect '/figures'
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+
+  post '/figures' do
+    # {figure: {name: 'some name'}}
+    @figure = Figure.create(name: params['figure']['name'])
+
+    if params["landmark"]["name"]
+      @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+    end
+
+    if params['title']['name']
+      @figure.titles << Title.create(params[:title])
+    end
+    binding.pry
+    @figure.save
+
+    redirect "/figures/#{@figure.id}"
+  end
 end
